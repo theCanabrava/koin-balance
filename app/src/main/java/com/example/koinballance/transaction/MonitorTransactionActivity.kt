ed60baa42b8b030c662b9980886cfa571dbab3b1
thecanabrava <vcanabravatr@gmail.com>
2022-07-07 14:13:40 -0300

    Monitor transaction layout

---
+++ app/src/main/java/com/example/koinballance/transaction/MonitorTransactionActivity.kt
@@ -0,0 +1,12 @@
+package com.example.koinballance.transaction
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.koinballance.R
+
+class MonitorTransactionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_monitor_transaction)
+    }
+}
