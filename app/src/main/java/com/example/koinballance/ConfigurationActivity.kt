01f1f787a2621c1b38cc12ff2cf903c3b1d0c893
thecanabrava <vcanabravatr@gmail.com>
2022-07-07 09:47:40 -0300

    ConfigurationActivity.kt functionality

--- app/src/main/java/com/example/koinballance/ConfigurationActivity.kt
+++ app/src/main/java/com/example/koinballance/ConfigurationActivity.kt
@@ -2,12 +2,51 @@ package com.example.koinballance
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.koinballance.component.UserSettings
+import com.example.koinballance.databinding.ActivityConfigurationBinding
+import org.koin.android.ext.android.inject
+import java.util.*
 
 class ConfigurationActivity : AppCompatActivity() {
+
+    private val userSettings: UserSettings by inject()
+    private lateinit var binding: ActivityConfigurationBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_configuration)
+        binding = ActivityConfigurationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         supportActionBar!!.title = getString(R.string.configuration)
+        bindSpinner()
+        getDataFromSettings()
+
+        binding.confirmChanges.setOnClickListener {
+            userSettings.changeName(binding.usernameField.text.toString())
+            val currencyCode = binding.currencyDropdown.selectedItem as String
+            userSettings.changeCurrency(Currency.getInstance(currencyCode))
+        }
+        binding.resetChanges.setOnClickListener { getDataFromSettings() }
+    }
+
+    private fun getDataFromSettings()
+    {
+        binding.usernameField.setText(userSettings.getSettings().name)
+        val position = resources.getStringArray(R.array.currencies)
+            .indexOf(userSettings.getSettings().currency.currencyCode)
+        binding.currencyDropdown.setSelection(position)
+    }
+
+    private fun bindSpinner()
+    {
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.currencies,
+            android.R.layout.simple_spinner_item
+        ).also {
+            it.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            binding.currencyDropdown.adapter = it
+        }
+
     }
 }
